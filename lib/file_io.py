32da99e0b7b2006c46bd499636e8b7a76ee526a4
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-22 20:17:06 -0500

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    # writes content to a .txt file, overwriting any existing content
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    # appends content to an existing .txt file
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    # reads and returns the content of a .txt file
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
